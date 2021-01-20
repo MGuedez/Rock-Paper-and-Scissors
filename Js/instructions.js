8adbd5c5016e16ebdac0727c29db182c72f0ccf9
Marianna Guedez <mariannaguedez@gmail.com>
2021-01-20 16:04:51 -0300

    Crea el funcionamiento para motrar el modal de instrucciones al hacer click en el boton

---
+++ Js/instructions.js
@@ -0,0 +1,8 @@
+let modal = document.getElementById("modal");
+
+// Abrir modal
+let openModal = document.getElementById("openModal");
+openModal.addEventListener("click" , open);
+function open() {
+    modal.style.display = "block";
+}
