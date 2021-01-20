03ed4fdcff9c85754192905a208c3b83a65ca298
Marianna Guedez <mariannaguedez@gmail.com>
2021-01-20 18:59:03 -0300

    Aplica el funcionamiento de abrir y cerrar el modal, Cambia los estilos de transición del modal

--- Js/instructions.js
+++ Js/instructions.js
@@ -4,5 +4,20 @@ let modal = document.getElementById("modal");
 let openModal = document.getElementById("openModal");
 openModal.addEventListener("click" , open);
 function open() {
-    modal.style.display = "block";
+    // modal.style.display = "block";
+    modal.style.transform = "translateY(0)";
+    modal.style.opacity = "1";
+    modal.style.visibility = "visible";
+}
+
+// Cerrar modal
+let closeModal = document.getElementById("close");
+closeModal.addEventListener("click" , close);
+function close() {
+    modal.style.transform = "translateY(-200%)";
+
+    setTimeout(function() {
+        modal.style.opacity = "0";
+        modal.style.visibility = "hidden";
+    }, 900);
 }
