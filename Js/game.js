a65fd43257f6c3034ef3c25c2c99bdef354a6d10
Marianna Guedez <mariannaguedez@gmail.com>
2021-01-21 18:56:00 -0300

    Crea las funciones de ganar y perder, Añade la funcionalidad de incrementar los puntos cuando se gana la jugada

--- Js/game.js
+++ Js/game.js
@@ -6,6 +6,13 @@ const buttonSurrender = document.getElementById("surrender");
 let movedMachine = document.getElementById("movementMachine");
 let movedUser = document.getElementById("movementUser");
 
+let notification = document.getElementById("notifications");
+
+let userScore = 0;
+let machineScore = 0;
+let userPoints = document.getElementById("scoreUser");
+let machinePoins = document.getElementById("scoreMachine");
+
 // ----------función principal del juego----------//
 function play() {
     buttonRock.addEventListener("click", () => game("rock"));
@@ -18,15 +25,10 @@ play();
 function game(option) {
     const motionPc = machineOption();
     const motionUser = option;
-    let optUser = document.getElementById("opt__user");
+    let optUser = document.getElementById("optUser");
 
     movedUser.style.display = "none";
     optUser.style.display = "block";
-
-    // roundGame = roundGame + 1;
-    // round.innerHTML = "Round"
-    // numberRound.style.display = "block";
-    // numberRound.innerHTML = "#" + roundGame;
 
     if (motionUser === "rock") {
         optUser.src = "https://i.postimg.cc/3RytWMHj/piedra.png";
@@ -43,11 +45,10 @@ function game(option) {
     } else if ((motionUser === "scissors") && (motionPc === "paper")) {
         win(motionUser, motionPc);
     } else if (motionUser === motionPc) {
-        tie();
+        notification.innerHTML = "Empate"
     } else {
         lose(motionUser);
     }
-    alert(motionUser);
 }
 
 // ----------funcion para obtener movimientos de la Pc----------//
@@ -55,7 +56,7 @@ function machineOption() {
     const availableOptions = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * 3);
     const motionMachine = availableOptions [random];
-    let optMachine = document.getElementById("opt__machine");
+    let optMachine = document.getElementById("optMachine");
 
     movedMachine.style.display = "none";
     optMachine.style.display = "block";
@@ -69,4 +70,21 @@ function machineOption() {
     }
 
     return motionMachine;
+}
+
+
+// ----------funcion usuario gana----------//
+function win() {
+    userScore = userScore + 1;
+    userPoints.innerHTML = userScore;
+
+    // stopGame()
+}
+
+// ----------funcion usuario pierde----------//
+function lose() {
+    machineScore = machineScore + 1;
+    machinePoins.innerHTML = machineScore;
+
+    // stopGame();
 }
